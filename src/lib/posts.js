63c501a9611678dd26f5d3e1c565e9de582eaf3f
Sintja <sporeci2000@gmail.com>
2025-08-10 18:11:04 -0400

    Posts created

---
+++ src/lib/posts.js
@@ -0,0 +1,20 @@
+export const mockBlogData = [
+  {
+    id: 1,
+    slug: "my-first-post",
+    title: "My First Post",
+    content: "This is my first blog post."
+  },
+  {
+    id: 2,
+    slug: "my-second-post",
+    title: "My second Post",
+    content: "This is my second blog post."
+  },
+  {
+    id: 3,
+    slug: "my-third-post",
+    title: "My third Post",
+    content: "This is my third blog post."
+  }
+];
