20a37e652d3de90a3509fbde0707ff70adca8f8c
Sintja <sporeci2000@gmail.com>
2025-08-10 18:35:30 -0400

    Admin

---
+++ src/pages/Admin.jsx
@@ -0,0 +1,8 @@
+export default function Admin() {
+    return (
+        <div style={{ padding: "1rem" }}>
+            <h1>Welcome to the Admin Dashboard</h1>
+            <p>This page is protected.</p>
+        </div>
+    );
+}
